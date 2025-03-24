d5933193d359ade57abd56e8d4e1f75d1c3f9b12
xiangjun_lee <lxj35570747@gmail.com>
2025-03-24 22:12:47 +0800

    初始化项目

---
+++ src/main/java/com/keji/green/lit/engine/GreenLitEngineApplication.java
@@ -0,0 +1,13 @@
+package com.keji.green.lit.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLitEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenLitEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keji/green/lit/engine/GreenLitEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keji.green.lit.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLitEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
