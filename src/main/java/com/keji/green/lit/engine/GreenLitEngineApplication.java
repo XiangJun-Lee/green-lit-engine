de7d5369958f3ba23d68da9e0a93cae9c5e1cd45
xiangjun_lee <lxj35570747@gmail.com>
2025-03-24 22:19:00 +0800

    init

--- src/main/java/com/keji/green/lit/engine/GreenLitEngineApplication.java
+++ src/main/java/com/keji/green/lit/engine/GreenLitEngineApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreenLitEngineApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(GreenLitEngineApplication.class, args);
     }
 
