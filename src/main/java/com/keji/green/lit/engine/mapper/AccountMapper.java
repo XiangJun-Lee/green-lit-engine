2e4f2b085565a5f1f5e76f0e9ee1d76066bccfc0
zhw <root1@192.168.1.7>
2025-04-27 23:40:19 +0800

    update

--- src/main/java/com/keji/green/lit/engine/mapper/AccountMapper.java
+++ src/main/java/com/keji/green/lit/engine/mapper/AccountMapper.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.keji.green.lit.engine.model.Account;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/keji/green/lit/engine/sharding/AccountAlgorithm.java
+++ src/main/java/com/keji/green/lit/engine/sharding/AccountAlgorithm.java
@@ -5,5 +5,33 @@ import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardi
 import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
 
 import java.util.*;
+
 public class AccountAlgorithm implements ComplexKeysShardingAlgorithm {
+    @Override
+    public Collection<String> doSharding(Collection collection, ComplexKeysShardingValue complexKeysShardingValue) {
+        String logicTableName = complexKeysShardingValue.getLogicTableName();
+        Collection<String> result = new ArrayList<>();
+        List<Long> ids = (List<Long>) complexKeysShardingValue.getColumnNameAndShardingValuesMap().get("user_id");
+        Long id = ids.get(0);
+//        List<Integer> gradeList = (List<Integer>) complexKeysShardingValue.getColumnNameAndShardingValuesMap().get("grade");
+//        Iterator<Integer> iterator = gradeList.iterator();
+//        while (iterator.hasNext()) {
+//            Integer grade = iterator.next();
+//            String tableName = logicTableName + ((id + grade) % 2 + 1);
+//            result.add(tableName);
+//        }
+        String tableName = logicTableName + (id  % 10);
+        result.add(tableName);
+        return result;
+    }
+
+    @Override
+    public Properties getProps() {
+        return null;
+    }
+
+    @Override
+    public void init(Properties properties) {
+
+    }
 }
