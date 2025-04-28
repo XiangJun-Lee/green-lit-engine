e17a8e5acd41604094392d811b0b48325525deff
xiangjun_lee <lxj35570747@gmail.com>
2025-04-28 14:39:11 +0800

    feat(engine): 整合 MyBatis 映射和更新数据源配置
    - 在 GreenLitEngineApplication 中添加多个 MapperScan 注解，扫描多个包
    - 更新 InterviewInfoMapper、UserInviteRelationMapper 和 UserMapper 的 XML 文件，指定完整的类名
    - 修改 application.properties 文件，更新数据库连接配置

--- src/main/java/com/keji/green/lit/engine/GreenLitEngineApplication.java
+++ src/main/java/com/keji/green/lit/engine/GreenLitEngineApplication.java
@@ -1,6 +1,7 @@
 package com.keji.green.lit.engine;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -13,7 +14,10 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @ImportResource("classpath:spring/green-lit-application.xml")
-@MapperScan("com.keji.green.lit.engine.model")
+@MapperScans({
+        @MapperScan("com.keji.green.lit.engine.model"),
+        @MapperScan("com.keji.green.lit.engine.mapper")
+})
 public class GreenLitEngineApplication {
 
     public static void main(String[] args) {
